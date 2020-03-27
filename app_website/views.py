ffee19422aa72cebe734ce01ab8f8c2ea2064b65
rishitaggarwal1 <rishitaggarwal1@gmail.com>
2020-03-28 00:19:13 +0530

    Completed All the Pages

--- app_website/views.py
+++ app_website/views.py
@@ -50,11 +50,11 @@ def subtract(request):
         if int(answer) == correct_ans:
             checker = "Correct!"
             alert = "alert-success"
-            statement = old_num_1 + " + " + old_num_2 + " = "+str(correct_ans)
+            statement = old_num_1 + " - " + old_num_2 + " = "+str(correct_ans)
         else:
             checker = "Incorrect!"
             alert = "alert-danger"
-            statement = old_num_1 + " + " + old_num_2 + " = "+str(correct_ans)
+            statement = old_num_1 + " - " + old_num_2 + " = "+str(correct_ans)
         return render(request, 'subtract.html', {
             'answer': answer,
             'checker': checker,
@@ -70,8 +70,64 @@ def subtract(request):
 
 
 def multiply(request):
-    return render(request, 'multiply.html', {})
+    from random import randint
+
+    num_1 = randint(0, 100)
+    num_2 = randint(0, 100)
+    if request.method == "POST":
+        old_num_1 = request.POST['num_1']
+        old_num_2 = request.POST['num_2']
+        correct_ans = int(old_num_1)*int(old_num_2)
+        answer = request.POST['answer']
+        if int(answer) == correct_ans:
+            checker = "Correct!"
+            alert = "alert-success"
+            statement = old_num_1 + " * " + old_num_2 + " = "+str(correct_ans)
+        else:
+            checker = "Incorrect!"
+            alert = "alert-danger"
+            statement = old_num_1 + " * " + old_num_2 + " = "+str(correct_ans)
+        return render(request, 'multiply.html', {
+            'answer': answer,
+            'checker': checker,
+            'alert': alert,
+            'statement': statement,
+            'num_1': num_1,
+            'num_2': num_2
+        })
+    return render(request, 'multiply.html', {
+        'num_1': num_1,
+        'num_2': num_2
+    })
 
 
 def divide(request):
-    return render(request, 'divide.html', {})
+    from random import randint
+
+    num_1 = randint(0, 100)
+    num_2 = randint(1, 100)
+    if request.method == "POST":
+        old_num_1 = request.POST['num_1']
+        old_num_2 = request.POST['num_2']
+        correct_ans = int(old_num_1)/int(old_num_2)
+        answer = request.POST['answer']
+        if int(answer) == correct_ans:
+            checker = "Correct!"
+            alert = "alert-success"
+            statement = old_num_1 + " / " + old_num_2 + " = "+str(correct_ans)
+        else:
+            checker = "Incorrect!"
+            alert = "alert-danger"
+            statement = old_num_1 + " / " + old_num_2 + " = "+str(correct_ans)
+        return render(request, 'divide.html', {
+            'answer': answer,
+            'checker': checker,
+            'alert': alert,
+            'statement': statement,
+            'num_1': num_1,
+            'num_2': num_2
+        })
+    return render(request, 'divide.html', {
+        'num_1': num_1,
+        'num_2': num_2
+    })
