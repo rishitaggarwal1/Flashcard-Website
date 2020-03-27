530b9b42f8e4729768f1134ef86895be50cf70f0
rishitaggarwal1 <rishitaggarwal1@gmail.com>
2020-03-27 23:43:47 +0530

    Add Part Completed

--- app_website/views.py
+++ app_website/views.py
@@ -16,12 +16,18 @@ def add(request):
         correct_ans = int(old_num_1)+int(old_num_2)
         answer = request.POST['answer']
         if int(answer) == correct_ans:
-            checker = "Correct!  "+old_num_1 + " + " + old_num_2 + " = "+answer
+            checker = "Correct!"
+            alert = "alert-success"
+            statement = old_num_1 + " + " + old_num_2 + " = "+str(correct_ans)
         else:
-            checker = "Incorrect! "+old_num_1 + " + " + old_num_2 + " is not = "+answer
+            checker = "Incorrect!"
+            alert = "alert-danger"
+            statement = old_num_1 + " + " + old_num_2 + " = "+str(correct_ans)
         return render(request, 'add.html', {
             'answer': answer,
             'checker': checker,
+            'alert': alert,
+            'statement': statement,
             'num_1': num_1,
             'num_2': num_2
         })
